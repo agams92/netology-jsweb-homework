c73c631d4297a65e8d68287e3f26038a02d710f5
Lev <agams92@gmail.com>
2018-04-21 13:58:38 +0300

    Minor changes

--- browser/gallery/js/buttons.js
+++ browser/gallery/js/buttons.js
@@ -3,17 +3,34 @@ let sources = ['i/breuer-building.jpg','i/guggenheim-museum.jpg','i/headquarters
 let index = 0;      
 let buttonNext = document.getElementById('nextPhoto');
 let buttonPrev = document.getElementById('prevPhoto');
-buttonNext.addEventListener('click', () => {
+
+// buttonNext.addEventListener('click', () => {
+//   index++;
+//   if(index > sources.length - 1) {
+//     index = 0;
+//   }
+//   document.getElementById('currentPhoto').src = sources[index];
+// })
+// buttonPrev.addEventListener('click',() => {
+//   index--;
+//   if(index < 0) {
+//     index = sources.length - 1;
+//   }
+//   document.getElementById('currentPhoto').src = sources[index];
+// })
+
+buttonNext.onclick = function () {
   index++;
   if(index > sources.length - 1) {
     index = 0;
   }
   document.getElementById('currentPhoto').src = sources[index];
-})
-buttonPrev.addEventListener('click',() => {
+}
+
+buttonPrev.onclick = function() {
   index--;
   if(index < 0) {
     index = sources.length - 1;
   }
   document.getElementById('currentPhoto').src = sources[index];
-})
+}
--- browser/slider/js/slider.js
+++ browser/slider/js/slider.js
@@ -1,5 +1,5 @@
 'use strict';
-let sources = ['i/airmax.png','i/airmax-jump.png','i/airmax-on-foot.png','i/airmax-playground.png','i/airmax-top-view.png'];
+let sources = ['i/airmax-jump.png','i/airmax-on-foot.png','i/airmax-playground.png','i/airmax-top-view.png','i/airmax.png'];
 let index = 0;
 setInterval(() => {
   if(index === sources.length) {
