d552c6d02357a1385936f8e9e778e5e2c44ddbc8
Lev <agams92@gmail.com>
2018-05-03 07:12:14 +0300

    Piano corrected

--- event-object/piano/js/script.js
+++ event-object/piano/js/script.js
@@ -8,24 +8,32 @@ function changeTone(event) {
     if (event.shiftKey) {
         for (let i = 0; i < players.length; i++) {
             players[i].src = 'sounds/lower' + sounds[i];
+            // console.log(players[i].getAttribute('src'));
         }
+        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
         piano.classList.remove('middle','higher');
         piano.classList.add('lower');
     } else if (event.altKey) {
         for (let i = 0; i < players.length; i++) {
             players[i].src = 'sounds/higher' + sounds[i];
+            // console.log(players[i].getAttribute('src'));
         }
+        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
         piano.classList.remove('middle','lower');
         piano.classList.add('higher');
     } 
 }
 
-function basicTone() {
-    for (let i = 0; i < players.length; i++) {
-        players[i].src = 'sounds/middle' + sounds[i];
+function basicTone(event) {
+    if (event.key === 'Shift' || event.key === 'Alt') {
+        for (let i = 0; i < players.length; i++) {
+            players[i].src = 'sounds/middle' + sounds[i];
+            // console.log(players[i].getAttribute('src'));
+        }
+        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
+        piano.classList.remove('lower','higher');
+        piano.classList.add('middle');
     }
-    piano.classList.remove('lower','higher');
-    piano.classList.add('middle');
 }
 
 function playMusic() {
