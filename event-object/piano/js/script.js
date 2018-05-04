91704697cb8b6951b0ddb71ff71575148c681543
Lev <agams92@gmail.com>
2018-05-04 09:52:14 +0300

    Transferred from keydown to click

--- event-object/piano/js/script.js
+++ event-object/piano/js/script.js
@@ -5,32 +5,20 @@ const piano = document.getElementsByTagName('ul')[0];
 const buttons = document.getElementsByTagName('li');
 
 function changeTone(event) {
+    let currentPlayer = this.getElementsByTagName('audio')[0];
+    let currentPlayerIndex = Array.from(players).findIndex((el) => el === currentPlayer);
+
     if (event.shiftKey) {
-        for (let i = 0; i < players.length; i++) {
-            players[i].src = 'sounds/lower' + sounds[i];
-            // console.log(players[i].getAttribute('src'));
-        }
-        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
+        currentPlayer.src = 'sounds/lower' + sounds[currentPlayerIndex];
         piano.classList.remove('middle','higher');
         piano.classList.add('lower');
     } else if (event.altKey) {
-        for (let i = 0; i < players.length; i++) {
-            players[i].src = 'sounds/higher' + sounds[i];
-            // console.log(players[i].getAttribute('src'));
-        }
-        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
+        currentPlayer.src = 'sounds/higher' + sounds[currentPlayerIndex];
         piano.classList.remove('middle','lower');
         piano.classList.add('higher');
-    } 
-}
-
-function basicTone(event) {
-    if (event.key === 'Shift' || event.key === 'Alt') {
-        for (let i = 0; i < players.length; i++) {
-            players[i].src = 'sounds/middle' + sounds[i];
-            // console.log(players[i].getAttribute('src'));
-        }
-        // console.log(`Клавиша - ${event.code}, тип - ${event.type}, event.repeat - ${event.repeat}`);
+    }
+    else {
+        currentPlayer.src = 'sounds/middle' + sounds[currentPlayerIndex];
         piano.classList.remove('lower','higher');
         piano.classList.add('middle');
     }
@@ -38,16 +26,10 @@ function basicTone(event) {
 
 function playMusic() {
     let player = this.getElementsByTagName('audio')[0];
-    if (player.currentTime > 0) {
-        player.load();
-        player.play();
-    } else {
-        player.play();
-    }
+    player.play();
 }
 
-document.addEventListener('keydown',changeTone);
-document.addEventListener('keyup',basicTone);
 for (let button of buttons) {
-    button.addEventListener('click',playMusic);
+    button.addEventListener('click', changeTone);
+    button.addEventListener('click', playMusic);
 }
