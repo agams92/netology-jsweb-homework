32af534eeeaa5ef11156b72d21dbb362048d7f52
Lev <agams92@gmail.com>
2018-05-17 16:05:31 +0300

    Table done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if (!event.target.classList.contains('prop__name')) return;
+    if(!event.target.dataset.dir) event.target.dataset.dir = -1;
+    if(event.target.dataset.dir == -1) event.target.dataset.dir = 1;
+    else event.target.dataset.dir = -1;
+    let dir = event.target.dataset.dir;
+    let name = event.target.dataset.propName;
+    document.getElementsByTagName('table')[0].dataset.sortBy = name;
+    sortTable(name,dir);
 }
