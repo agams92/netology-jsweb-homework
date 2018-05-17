11d7183c9152da2cc3a455bbf1ecc7e3a656511e
Lev <agams92@gmail.com>
2018-05-17 16:42:39 +0300

    Shop done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.getElementsByClassName('items-list')[0].addEventListener('click', buttonAdd);
+
+function buttonAdd() {
+    console.log(event.currentTarget)
+    if (!event.target.classList.contains('add-to-cart')) return;
+    else {
+        let item = {};
+        item.title = event.target.dataset.title;
+        item.price = event.target.dataset.price;
+        addToCart(item);
+    }
+    
+}
