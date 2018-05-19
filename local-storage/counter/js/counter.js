8d6a04af8a4608e9388b709df96e0390c150dd78
Lev <agams92@gmail.com>
2018-05-19 20:58:12 +0300

    Counter done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,19 @@
 'use strict';
+const plus = document.getElementById('increment');
+const minus = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+const counter = document.getElementById('counter');
+
+document.getElementsByClassName('wrap-btns')[0].addEventListener('click', count);
+
+if(!localStorage.counter) {
+    localStorage.counter = 0;
+}
+counter.innerText = localStorage.counter;
+
+function count(event) {
+    if (event.target == plus) localStorage.counter = Number(localStorage.counter) + 1;
+    else if(event.target == minus) localStorage.counter = Number(localStorage.counter) - 1;
+    else if(event.target == reset) localStorage.counter = 0;
+    counter.innerText = localStorage.counter;
+}
