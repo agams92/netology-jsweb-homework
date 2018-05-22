762bdcfe122fe6c072f8b88cbd5cdfcb38629fdd
Lev <agams92@gmail.com>
2018-05-22 16:35:02 +0300

    Counter done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,14 @@
 'use strict';
+const openConnectionsCount = document.getElementsByClassName('counter')[0];
+const errorConnectionsCount = document.querySelector('output.errors');
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('open', event => console.log('Соединение открыто'));
+connection.addEventListener('message', event => {
+    let data = JSON.parse(event.data);
+    openConnectionsCount.innerText = data.connections;
+    errorConnectionsCount.innerText = data.errors;
+})
+window.addEventListener('beforeunload', event => {
+    connection.close(1000, 'WASTED');
+})
