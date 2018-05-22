bf564c0cab0088391886509481863ec4ef7387f1
Lev <agams92@gmail.com>
2018-05-22 16:16:55 +0300

    Bubbles half-done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,13 @@
 'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+function sendCoordinates() {
+    let coordinates = {};
+    coordinates.pageX = event.pageX;
+    coordinates.pageY = event.pageY;
+    connection.send(JSON.stringify(coordinates));
+}
+
+connection.addEventListener('open', event => showBubbles(connection));
+connection.addEventListener('message', event => showBubbles(connection));
+connection.addEventListener('error', event => console.log('Error= ' + event.error));
