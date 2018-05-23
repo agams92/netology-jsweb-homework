909e5d6a9963a5f6f7f666bca276ad8d8055d5fa
Lev <agams92@gmail.com>
2018-05-23 15:47:06 +0300

    Test changes

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -10,16 +10,17 @@ signUpButton.addEventListener('click', event => authRequest('https://neto-api.he
 function authRequest(url, dataSource) {
     event.preventDefault();
     let formData = new FormData(dataSource);
-    let form = {};
-    for (const [key, value] of formData) {
-        form[key] = value;
-    }
+    // let form = {};
+    // for (const [key, value] of formData) {
+    //     form[key] = value;
+    // }
     let output = dataSource.getElementsByClassName('error-message')[0];
     let xhr = new XMLHttpRequest();
     try {
         xhr.addEventListener('load',(e) => {
             if(200 <= xhr.status && xhr.status < 300) {
                 let data = JSON.parse(xhr.responseText);
+                console.log(data);
                 if (dataSource === signInForm) {
                     if (data.error) {
                         output.value = data.message;
@@ -37,7 +38,7 @@ function authRequest(url, dataSource) {
         })
         xhr.open('POST', url);
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify(form));
+        xhr.send(JSON.stringify(formData));
     } catch (error) {
         output.value = error.message;
     }
