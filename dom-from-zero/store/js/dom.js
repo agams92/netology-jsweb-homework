4fb435aadb79bd25cb7c4c7608cc59ed5208c162
Lev <agams92@gmail.com>
2018-05-25 18:06:30 +0300

    Store done (works in Codepen)

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,17 @@
 'use strict';
+function createElement(node) {
+    if ((typeof node === 'string') || (typeof node === 'number') || (node === true)) return document.createTextNode(node);
+    if (Array.isArray(node)) {
+        return node.reduce((fragment, elem) => {
+            fragment.appendChild(createElement(elem));
+            return fragment;
+        }, document.createDocumentFragment());
+    }
+    let element = document.createElement(node.name);
+    if(node.props){
+        Object.keys(node.props).forEach(key => element.setAttribute(key, node.props[key]));
+    }
+    element.appendChild(createElement(node.childs));
+    console.log(element)
+    return element;
+}
