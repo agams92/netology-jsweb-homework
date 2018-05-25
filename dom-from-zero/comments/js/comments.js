1ae6e328a17632307e5479280f6bf8ab3906480f
Lev <agams92@gmail.com>
2018-05-25 12:00:54 +0300

    Comments done

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -2,28 +2,68 @@
 
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+  commentsContainer.appendChild(insertBlock(list.map(comment => createComment(comment))));
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+  return {
+    tag: 'div', cls: 'comment-wrap',
+    content: [
+      {tag: 'div', cls: 'photo',
+        attrs: {title: comment.author.name},
+        content: {tag: 'div', cls: 'avatar',
+                  attrs: {style: `background-image: url('${comment.author.pic}`}
+        }
+      },
+      {
+        tag: 'div', cls: 'comment-block',
+        content: [
+          {tag: 'p', cls: 'comment-text', txt: comment.text},
+          {tag: 'div', cls: 'bottom-comment',
+            content: [
+              {tag: 'div', cls: 'comment-date', txt: new Date(comment.date).toLocaleString('ru-Ru')},
+              {tag: 'ul', cls: 'comment-actions',
+                content: [
+                  {tag: 'li', cls: 'complain', txt: 'Пожаловаться'},
+                  {tag: 'li', cls: 'reply', txt: 'Ответить'}
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+
+function insertBlock(block) {
+    if ((block === undefined) || (block === null) || (block === false)) {
+        return document.createTextNode('');
+    }
+
+    if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
+        return document.createTextNode(block);
+    }
+
+    if (Array.isArray(block)) {
+        return block.reduce((f, elem) => {
+            f.appendChild(insertBlock(elem));
+            return f;
+        }, document.createDocumentFragment());
+    }
+
+    let element = document.createElement(block.tag || 'div');
+    [].concat(block.cls || [])
+        .forEach(className => element.classList.add(className));
+    if (block.attrs) {
+        Object.keys(block.attrs)
+            .forEach(key => element.setAttribute(key, block.attrs[key]))
+    }
+    if (block.txt) {
+      element.innerText = block.txt;
+    }
+    element.appendChild(insertBlock(block.content));
+    return element;
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
