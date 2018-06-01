feb8868b3d58c7c7be55fdbdc35edfea6dfa4331
Lev <agams92@gmail.com>
2018-06-01 17:57:18 +0300

    Draw half-done

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,11 @@
 'use strict';
+let ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+window.editor.addEventListener('update', check);
+
+function check(){
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext('2d');
+    let image = context.getImageData(0,0,canvas.width,canvas.height);
+    let binary = Uint8Array.from(image.data);
+    ws.send(binary.buffer);
+}
